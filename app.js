24f9a4697467bbb3ddd3b38235ef40cb21f1a857
akshay <akshayexazad@gmail.com>
2022-11-27 18:37:19 +0530

    route functionality

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 // let http = require('http');
 const express = require('express')
 const app = express();
-const Bodyparser=require('body-parser');
 const bodyParser = require('body-parser');
+
+const adminRoute=require('./route/admin');
+
+const shoproute= require('./route/shop')
+
 
 // let route = require('./router')
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/my-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"> <input type= "text" name="title"> <input type= "text" name="title"><button type="submit">Add Product</button></form>')
-});
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-
+app.use('/admin',adminRoute);
+app.use(shoproute);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>')
 })
-app.use('/',(req,res,next)=>{
-    res.send('<h1>hello from Express js i am in another middlewarw</h1>')
-});
 
 app.listen(5001);
 
