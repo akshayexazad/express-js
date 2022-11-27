60ca78c7edca66dd3a9669170ff2dec06f547d30
akshay <akshayexazad@gmail.com>
2022-11-27 17:18:06 +0530

    body parser

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // let http = require('http');
 const express = require('express')
 const app = express();
+const Bodyparser=require('body-parser');
+const bodyParser = require('body-parser');
 
 // let route = require('./router')
-app.use((req,res,next)=>{
-    console.log("i am in middlewere1")
-    next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/my-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"> <input type= "text" name="title"> <input type= "text" name="title"><button type="submit">Add Product</button></form>')
 });
-app.use((req,res,next)=>{
-    console.log("i am in middlewere2")
-    res.send('<h1>hello from Express js</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello from Express js i am in another middlewarw</h1>')
 });
 
 app.listen(5001);
